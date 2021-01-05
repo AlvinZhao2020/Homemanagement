d1e0f79f49e465004d61bb967f2a5b1e46d6d57c
alvin zhao <yibozhao2018@gmail.com>
2021-01-04 21:20:05 -0800

    validates house

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,13 @@
 class House < ApplicationRecord
+    # validation 
+    # validates will help avoid 500 error. not a database error but a ruby error
+    # t.string "address", null: false
+    # t.integer "buyin_price", null: false
+    # t.integer "current_price", null: false
+    # t.integer "rent", null: false
+    # t.integer "ownner_id", null: false
+    # t.datetime "created_at", null: false
+    # t.datetime "updated_at", null: false
+    validates :address, :buyin_price, :current_price,:rent, :ownner_id , presence: true 
+    
 end
